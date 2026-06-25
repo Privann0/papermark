836e6f61a3f6387db494ca066b369f569cfff03b
Privann0 <109453703+Privann0@users.noreply.github.com>
2026-06-25 16:03:23 +0200

    Update index.ts

--- lib/featureFlags/index.ts
+++ lib/featureFlags/index.ts
@@ -1,69 +1,40 @@
-import { get } from "@vercel/edge-config";
-
 export type BetaFeatures =
-  | "tokens"
-  | "incomingWebhooks"
-  | "roomChangeNotifications"
-  | "webhooks"
-  | "conversations"
-  | "dataroomUpload"
-  | "inDocumentLinks"
-  | "usStorage"
-  | "dataroomIndex"
-  | "slack"
-  | "annotations"
-  | "dataroomInvitations"
-  | "workflows"
-  | "ai"
-  | "sso"
-  | "textSelection";
+ | "tokens"
+ | "incomingWebhooks"
+ | "roomChangeNotifications"
+ | "webhooks"
+ | "conversations"
+ | "dataroomUpload"
+ | "inDocumentLinks"
+ | "usStorage"
+ | "dataroomIndex"
+ | "slack"
+ | "annotations"
+ | "dataroomInvitations"
+ | "workflows"
+ | "ai"
+ | "sso"
+ | "textSelection";
 
 type BetaFeaturesRecord = Record<BetaFeatures, string[]>;
 
 export const getFeatureFlags = async ({ teamId }: { teamId?: string }) => {
-  const teamFeatures: Record<BetaFeatures, boolean> = {
-    tokens: false,
-    incomingWebhooks: false,
-    roomChangeNotifications: false,
-    webhooks: false,
-    conversations: false,
-    dataroomUpload: false,
-    inDocumentLinks: false,
-    usStorage: false,
-    dataroomIndex: false,
-    slack: false,
-    annotations: false,
-    dataroomInvitations: false,
-    workflows: false,
-    ai: false,
-    sso: false,
-    textSelection: false,
-  };
-
-  // Return all features as false if edge config is not available
-  if (!process.env.EDGE_CONFIG) {
-    return Object.fromEntries(
-      Object.entries(teamFeatures).map(([key, _v]) => [key, false]),
-    );
-  } else if (!teamId) {
-    return teamFeatures;
-  }
-
-  let betaFeatures: BetaFeaturesRecord | undefined = undefined;
-
-  try {
-    betaFeatures = await get("betaFeatures");
-  } catch (e) {
-    console.error(`Error getting beta features: ${e}`);
-  }
-
-  if (betaFeatures) {
-    for (const [featureFlag, teamIds] of Object.entries(betaFeatures)) {
-      if (teamIds.includes(teamId)) {
-        teamFeatures[featureFlag as BetaFeatures] = true;
-      }
-    }
-  }
-
-  return teamFeatures;
+ return {
+   tokens: true,
+   incomingWebhooks: true,
+   roomChangeNotifications: true,
+   webhooks: true,
+   conversations: true,
+   dataroomUpload: true,
+   inDocumentLinks: true,
+   usStorage: false,
+   dataroomIndex: true,
+   slack: true,
+   annotations: true,
+   dataroomInvitations: true,
+   workflows: true,
+   ai: false,
+   sso: false,
+   textSelection: true,
+ };
 };
